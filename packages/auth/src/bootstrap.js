cc7014db9102e472539be6c6c9115eb52daa7e65
Venkat Jandhyala <venkat.jandhyala09@gmail.com>
2023-02-12 15:06:46 +0000

    Changes for auth login/signup

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath]
   });
@@ -12,7 +12,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     history.listen(onNavigate);
 
   ReactDOM.render(
-    <App history={history}/>,
+    <App history={history} onSignIn={onSignIn}/>,
     el
   );
 

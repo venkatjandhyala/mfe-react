149dfe1acd876ccbe9da92f4efcf786a743090c6
Venkat Jandhyala <venkat.jandhyala09@gmail.com>
2023-02-12 14:38:41 +0000

    filename changes

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -105,7 +105,7 @@ export default function SignIn({ onSignIn }) {
           </Button>
           <Grid container>
             <Grid item>
-              <Link to="/auth/signup">{"Don't have an account? Sign Up"}</Link>
+              <Link to="/auth/signup">{"Do not have an account? Sign Up"}</Link>
             </Grid>
           </Grid>
         </form>
--- packages/auth/src/components/Signup.js
+++ packages/auth/src/components/Signup.js
@@ -130,7 +130,7 @@ export default function SignUp({ onSignIn }) {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link to="/auth/signin">Already have an account? Sign in</Link>
+              <Link to="/auth/signin">Already have an account? Sign In</Link>
             </Grid>
           </Grid>
         </form>
